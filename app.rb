baa6eb0ad79821b4449ee8524c72b10be277b535
Jesper Thomschütz <jesper@jespersaur.com>
2013-01-27 23:32:00 +0000

    A very big backport of changes made via focusstreak. (sorry)
    * Delete account
    * Edit account
    * Reset password
    * Small cosmetic fixes
    * ...and more

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
-require 'rubygems'
-require 'sinatra'
-require 'sinatra/base'
-require 'mongo_mapper'
-require 'sinatra-authentication'
-require 'haml'
-require 'rack/csrf'
+class DeleteReason
+  include MongoMapper::Document
+
+  key :email, String, :required => true
+  key :reason, String, :required => true
+  key :account_age_in_days, Float, :required => true
+  key :account_created, Time, :required => true
+
+  timestamps!
+end
+
 
 class MmUser
   include MongoMapper::Document
+
   validates_presence_of :email
 end
 
 
 class Seedling < Sinatra::Base
   set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + 'views/'
+
+  configure :development do
+    require "sinatra/reloader"
+    register Sinatra::Reloader
+  end
 
   get '/' do
     @users = User.all
@@ -25,6 +35,88 @@ class Seedling < Sinatra::Base
     haml :login
   end
 
+  post '/login' do
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user] = user.id
+
+      if session[:return_to]
+        redirect_url = session[:return_to]
+        session[:return_to] = false
+        redirect redirect_url
+      else
+        redirect '/'
+      end
+    else
+      flash[:error] = 'Wrong Email or Password'
+      @email = params[:email]
+      haml :login
+    end
+  end
+
+  get '/forgot_password' do
+    @page_title = "Forgot Password"
+    haml :forgot_password
+  end
+
+  def send_email(to, subject, body)
+      Pony.mail(:to => to,
+                :subject => "[#{settings.project_name}] #{subject}",
+                :body => body)
+  end
+
+  post '/forgot_password' do
+    user = User.get(:email => params['email'])
+    if user.nil?
+      flash[:error] = "'#{h(params[:email])}' does not have an account here."
+      redirect '/forgot_password'
+    else
+      secret = User.random_string(20)
+      user.set(:secret => secret)
+      send_email(user.email,
+                 "Password Reset",
+                 "You may reset your password here: http://#{request.host}/reset_password/#{secret}")
+
+      @page_title = "Login"
+      flash[:notice] = "Check your email! Instructions have been sent to #{user.email}"
+      redirect '/'
+    end
+  end
+
+  get '/reset_password/:secret' do
+    if not User.get(:secret => params[:secret])
+      flash[:error] = "Recovery code '#{h(params[:secret])}' has expired or does not exist"
+      redirect '/'
+    else
+      @page_title = "Reset Password"
+      haml :reset_password
+    end
+  end
+
+  post '/reset_password' do
+    user = User.get(:secret => params[:secret])
+    if user.nil?
+      flash[:error] = "Recovery code '#{h(params[:secret])}' has expired or does not exist"
+      redirect '/'
+    end
+
+    password = params[:password]
+    confirmation = params[:confirmation]
+
+    if password.empty?
+      flash[:error] = "Password may not be blank"
+      redirect back
+    elsif password != confirmation
+      flash[:error] = "Password does not match confirmation"
+      redirect back
+    else
+      flash[:notice] = "Password has now been changed."
+      user.set(:hashed_password => User.encrypt(password, user.salt))
+      session[:user] = user.id
+      user.set(:secret => nil)
+      redirect '/'
+    end
+  end
+
   get '/signup' do
     if logged_in?
       redirect '/'
@@ -34,58 +126,92 @@ class Seedling < Sinatra::Base
   end
 
   post '/signup' do
-    if session[:user]
-      redirect '/'
-    end
+    user_params = params[:user]
 
     if not params.has_key?('tos')
-      @email = params[:user][:email]
-      @error = 'You must accept the Terms and Conditions'
+      @email = user_params[:email]
+      flash[:error] = 'You must accept the Terms and Conditions'
       return haml :signup
     end
 
-    @user = User.set(params[:user])
+    @user = User.set(user_params)
 
     if @user.valid && @user.id
       session[:user] = @user.id
       redirect '/'
     else
-      @error =  "#{@user.errors}."
+      @email = user_params[:email]
+      flash[:error] =  "#{@user.errors}."
       haml :signup
     end
   end
 
-  get '/settings/?:id?' do
+  get '/delete_account' do
     login_required
-    redirect '/' unless current_user.id.to_s == params[:id] || current_user.admin?
+    @page_title = "Delete Account"
+    haml :delete_account
+  end
 
-    if params[:id]
-      @user = User.get(:id => params[:id])
+  post '/delete_account' do
+    login_required
+
+    if not User.authenticate(current_user.email, params[:password])
+      @reason = params[:reason]
+      flash[:error] = "Incorrect Password"
+      redirect '/delete_account'
+    end
+
+    DeleteReason.create(:email => current_user.email,
+                        :reason => params[:reason],
+                        :account_created => current_user.created_at,
+                        :account_age_in_days => ((Time.now - current_user.created_at) / 60 / 60 / 24).round(3))
+
+    current_user.destroy
+    session[:user] = nil
+    redirect '/'
+  end
+
+  get '/settings' do
+    login_required
+    @user = current_user
+    haml :settings
+  end
+
+  post '/settings' do
+    login_required
+
+    user_attributes = params[:user]
+
+    password = params[:old_password]
+    salt = current_user.salt
+
+    user_attributes = params[:user]
+
+    if user_attributes[:password] == ""
+      user_attributes.delete("password")
+      user_attributes.delete("password_confirmation")
+      user_attributes[:password] = params[:old_password]
+    end
+
+    user = current_user
+
+    if not User.authenticate(user.email, password)
+      flash[:error] = "'Current password' was incorrect"
+    elsif user.update(user_attributes)
+      flash[:notice] = "Settings updated."
     else
-      @user = current_user
+      flash[:error] = user.errors
     end
 
     haml :settings
   end
 
-  post '/settings/?:id?' do
-    login_required
-    redirect "/" unless current_user.admin? || current_user.id.to_s == params[:id]
-    user = User.get(:id => params[:id])
-    user_attributes = params[:user]
+  # Kept at the bottom so we can overwrite default routes
+  register Sinatra::SinatraAuthentication
 
-    if params[:user][:password] == ""
-        user_attributes.delete("password")
-        user_attributes.delete("password_confirmation")
-    end
-
-    if user.update(user_attributes)
-      redirect '/'
-    else
-      @error = "There were some problems with your updates: #{user.errors}."
-      redirect "/settings/#{user.id}?"
+  helpers do
+    def h(text)
+      Rack::Utils.escape_html(text)
     end
   end
-
-  register Sinatra::SinatraAuthentication
 end
--- config.ru
+++ config.ru
@@ -1,6 +1,9 @@
 require 'bundler'
 Bundler.require
 
+require 'rack/csrf'
+require 'rack-flash'
+
 require './app'
 
 Seedling.set :project_name, 'Seedling'
@@ -9,9 +12,23 @@ Seedling.set :google_analytics, ENV['GOOGLE_ANALYTICS']
 use Rack::Session::EncryptedCookie, :expire_after => 3600*24*60, :secret => ENV['COOKIE_SECRET']
 use Rack::Csrf, :raise => true
 
+use Rack::Flash, :sweep => true
+
 logger = Logger.new($stdout)
 MongoMapper.connection = Mongo::Connection.new(ENV['DATABASE_HOST'], ENV['DATABASE_PORT'], :logger => logger)
 MongoMapper.database = ENV['DATABASE']
 MongoMapper.database.authenticate(ENV['DATABASE_USER'], ENV['DATABASE_PASSWORD'])
+
+Pony.options = { :from => "#{Seedling.project_name} <#{ENV['EMAIL_FROM']}>",
+                 :via => :smtp,
+                 :via_options => {
+                   :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+                   :address => ENV['EMAIL_HOST'],
+                   :user_name => ENV['EMAIL_USER'],
+                   :password => ENV['EMAIL_PASSWORD'],
+                   :authentication => :login,
+                   :port => 587,
+                   :enable_starttls_auto => false
+               } }
 
 run Seedling
